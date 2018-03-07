2f8404342c0c0fe876361eb9fc0c7d51a8685baa
Christian Bogen <christian.bogen@tik.uni-stuttgart.de>
2018-03-07 13:10:40 +0100

    Test mit local.js

--- config/local/local.js
+++ config/local/local.js
@@ -27,6 +27,9 @@
  *  limitations under the License.
  */
 
+MathJax.Hub.Config({
+  menuSettings: {zoom: "Double-Click"}
+});
 
 MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
   var TEX = MathJax.InputJax.TeX;
